74eef5590018d87bb4aee919a2f2d2e44675532c
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2021-01-24 13:23:04 +0300

    version 0.0.2-alpha hotfix

---
+++ index.ts
@@ -0,0 +1,2 @@
+import * as ALL from './src'
+
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Tag } from './lib/tag';
+export { RootSld } from './lib/root-sld';
+export { EasyTagCreator } from './lib/easy-tag-creator';
+
